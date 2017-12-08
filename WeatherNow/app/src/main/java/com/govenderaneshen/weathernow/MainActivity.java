a98d1ae88a3fe401f916acfc301cc49f1cb260a6
GovenderAneshen <34270401+GovenderAneshen@users.noreply.github.com>
2017-12-08 23:41:34 +0200

    Working on layout of main screen: Layout of components complete

--- WeatherNow/app/src/main/java/com/govenderaneshen/weathernow/MainActivity.java
+++ WeatherNow/app/src/main/java/com/govenderaneshen/weathernow/MainActivity.java
@@ -35,8 +35,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_main);
 
     }
 
