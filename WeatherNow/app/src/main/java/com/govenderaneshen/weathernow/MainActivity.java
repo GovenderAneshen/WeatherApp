cee7396db7222b84f2127740db33b4ce995c45e5
GovenderAneshen <34270401+GovenderAneshen@users.noreply.github.com>
2018-01-18 08:18:24 +0200

    Final changes to Forecast Table (Colour etc)

--- WeatherNow/app/src/main/java/com/govenderaneshen/weathernow/MainActivity.java
+++ WeatherNow/app/src/main/java/com/govenderaneshen/weathernow/MainActivity.java
@@ -2,6 +2,8 @@
 package com.govenderaneshen.weathernow;
 
 import android.Manifest;
+import android.app.AlertDialog;
+import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.location.Location;
 import android.os.Bundle;
@@ -10,19 +12,27 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.ContextThemeWrapper;
+import android.view.Gravity;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Locale;
 
 /**
  * @author Govender Aneshen
  * Main Activity class which excecutes code to output the current Weather to the user
  */
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AccessData{
 
     /*
      * Global Variables
@@ -35,20 +45,83 @@ public class MainActivity extends AppCompatActivity {
     static TextView date;
     static TableLayout tblForecast;
     static ProgressBar load;
-    static String stringWeatherData;
 
+ 
 
+    /*
+        Location variables
+     */
     private double currentLongitude;
     private double currentLatitude;
 
-
-
-    /*
-     * code value assigned to the different weather condition icons
+    /**
+     *
+     * @param imgCondition Is the Imageview for which the resource must be set
+     * @param conditionsCode is the weather conditions code
      */
-    private String conditionCode = " ";
+    public void setImage(ImageView imgCondition, String conditionsCode)
+    {
+        /*
+         * Switch statement to allocate the correct icon based on the weather condition
+         */
 
-
+        switch (conditionsCode) {
+            case "01d":
+                imgCondition.setImageResource(R.drawable.sun);
+                break;
+            case "01n":
+                imgCondition.setImageResource(R.drawable.moon);
+                break;
+            case "02d":
+                imgCondition.setImageResource(R.drawable.daycloud);
+                break;
+            case "02n":
+                imgCondition.setImageResource(R.drawable.nightcloud);
+                break;
+            case "03d":
+                imgCondition.setImageResource(R.drawable.cloud);
+                break;
+            case "03n":
+                imgCondition.setImageResource(R.drawable.cloud);
+                break;
+            case "04d":
+                imgCondition.setImageResource(R.drawable.brokencloud);
+                break;
+            case "04n":
+                imgCondition.setImageResource(R.drawable.brokencloud);
+                break;
+            case "09d":
+                imgCondition.setImageResource(R.drawable.showers);
+                break;
+            case "09n":
+                imgCondition.setImageResource(R.drawable.showers);
+                break;
+            case "10d":
+                imgCondition.setImageResource(R.drawable.rainday);
+                break;
+            case "10n":
+                imgCondition.setImageResource(R.drawable.rainnight);
+                break;
+            case "11d":
+                imgCondition.setImageResource(R.drawable.thunder);
+                break;
+            case "11n":
+                imgCondition.setImageResource(R.drawable.thunder);
+                break;
+            case "13d":
+                imgCondition.setImageResource(R.drawable.snow);
+                break;
+            case "13n":
+                imgCondition.setImageResource(R.drawable.snow);
+                break;
+            case "50d":
+                imgCondition.setImageResource(R.drawable.mist);
+                break;
+            case "50n":
+                imgCondition.setImageResource(R.drawable.mist);
+                break;
+        }
+    }
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
@@ -79,10 +152,10 @@ public class MainActivity extends AppCompatActivity {
             currentLongitude = location.getLongitude();
             currentLatitude = location.getLatitude();
 
-            RequestWeatherData weatherData = new RequestWeatherData(getApplicationContext());
+            RequestWeatherData weatherData = new RequestWeatherData(MainActivity.this);
             String url = "http://api.openweathermap.org/data/2.5/forecast/daily?lat="+String.valueOf(currentLatitude)+"&lon="+String.valueOf(currentLongitude)+"&cnt=6&appid=927d09bc49dbee6aac7f5cb1df707542";
             weatherData.execute(url);
-            Toast.makeText(getApplicationContext(),stringWeatherData,Toast.LENGTH_LONG).show();
+           
 
         }
         else
@@ -146,4 +219,239 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public void returnData(weather[] forecast, String areaName)
+    {
+         /*
+                Setting text in textViews
+             */
+        try {
+
+
+            minTempView.setText("Min:  " + forecast[0].getTempMin() + " °C");
+            maxTempView.setText("Max: " + forecast[0].getTempMax() + " °C");
+            ConditionView.setText(forecast[0].getWeatherDescription());
+            AreaView.setText(areaName);
+            setImage(MainActivity.condition, forecast[0].getConditionsCode());
+
+            SimpleDateFormat dateFormatday = new SimpleDateFormat("dd-MM-yyyy HH:mm", Locale.getDefault());
+            dateFormatday.applyPattern("EEEE, dd MMM yyyy");
+            date.setText(dateFormatday.format(forecast[0].getDate()));
+
+
+                /*
+                    Dealing with the forecast for 5 days
+                 */
+            tblForecast.removeAllViewsInLayout();
+            tblForecast.setStretchAllColumns(true);
+            tblForecast.setGravity(Gravity.CENTER);
+
+            TableRow tr = new TableRow(MainActivity.this);
+            TableRow tr2 = new TableRow(MainActivity.this);
+            for (int i = 1; i < forecast.length; i++)
+            {
+
+
+                    /*
+                        Storing data from the forecast array into local variables
+                     */
+                final String Day = forecast[i].getDay();
+                SimpleDateFormat dateFormatday2 = new SimpleDateFormat("dd-MM-yyyy HH:mm", Locale.getDefault());
+                dateFormatday2.applyPattern("dd MMM");
+                final String Date = dateFormatday2.format(forecast[i].getDate());
+                final String icon = forecast[i].getConditionsCode();
+                final String Description = forecast[i].getWeatherDescription();
+                final String MinTemp = "Min:  " + forecast[i].getTempMin() + " °C";
+                final String MaxTemp = "Max: " + forecast[0].getTempMax() + " °C";
+                int AverageTemp = (forecast[i].getTempMin() + forecast[i].getTempMax()) / 2;
+
+                TextView day = new TextView(MainActivity.this);
+                day.setText(Day);
+                day.setPadding(10, 5, 5, 5);
+                day.setTextColor(Color.WHITE);
+                day.setBackgroundColor(Color.parseColor("#80002afe"));
+                day.setGravity(Gravity.CENTER);
+                day.setTextSize(14);
+
+                TextView date2 = new TextView(MainActivity.this);
+                date2.setText(Date);
+                date2.setPadding(10, 5, 5, 5);
+                date2.setTextColor(Color.WHITE);
+                date2.setBackgroundColor(Color.parseColor("#80002afe"));
+                date2.setGravity(Gravity.CENTER);
+                date2.setTextSize(16);
+
+
+                TextView temp = new TextView(MainActivity.this);
+                temp.setText(AverageTemp + " °C");
+                temp.setPadding(10, 5, 5, 5);
+                temp.setTextColor(Color.BLACK);
+                temp.setGravity(Gravity.CENTER);
+                temp.setTextSize(18);
+
+                TextView description = new TextView(MainActivity.this);
+                description.setText(Description);
+                description.setPadding(10, 5, 5, 5);
+                description.setTextColor(Color.BLACK);
+                description.setGravity(Gravity.CENTER);
+                description.setTextSize(16);
+
+
+                ImageView iconCondition = new ImageView(MainActivity.this);
+
+                iconCondition.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT));
+                setImage(iconCondition, icon);
+
+                ViewGroup.LayoutParams layoutParams = iconCondition.getLayoutParams();
+                layoutParams.width = 120;
+                layoutParams.height = 120;
+                iconCondition.setLayoutParams(layoutParams);
+
+
+                final LinearLayout linearLayout = new LinearLayout(MainActivity.this);
+                linearLayout.setOrientation(LinearLayout.VERTICAL);
+                linearLayout.addView(day);
+                linearLayout.addView(date2);
+
+                final View vline1 = new View(MainActivity.this);
+                vline1.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.FILL_PARENT, 1));
+                vline1.setBackgroundColor(Color.WHITE);
+                linearLayout.addView(vline1);
+
+                linearLayout.addView(temp);
+                linearLayout.addView(iconCondition);
+
+                final View vline2 = new View(MainActivity.this);
+                vline2.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.FILL_PARENT, 1));
+                vline2.setBackgroundColor(Color.WHITE);
+
+                linearLayout.addView(vline2);
+
+                linearLayout.setGravity(Gravity.CENTER);
+                linearLayout.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        final LinearLayout linearLayout = new LinearLayout(MainActivity.this);
+                        linearLayout.setOrientation(LinearLayout.VERTICAL);
+
+                        TextView min = new TextView(MainActivity.this);
+                        min.setText(MinTemp);
+                        min.setPadding(10, 5, 5, 5);
+                        min.setTextColor(Color.WHITE);
+                        min.setGravity(Gravity.CENTER);
+                        min.setTextSize(18);
+
+                        TextView max = new TextView(MainActivity.this);
+                        max.setText(MaxTemp);
+                        max.setPadding(10, 5, 5, 5);
+                        max.setTextColor(Color.WHITE);
+                        max.setGravity(Gravity.CENTER);
+                        max.setTextSize(18);
+
+                        final LinearLayout linearLayout2 = new LinearLayout(MainActivity.this);
+                        linearLayout2.setOrientation(LinearLayout.VERTICAL);
+
+                        final LinearLayout verticalBorder = new LinearLayout(MainActivity.this);
+                        verticalBorder.setBackgroundColor(Color.WHITE);
+
+                        ImageView iconCondition2 = new ImageView(MainActivity.this);
+
+                        iconCondition2.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT));
+                        setImage(iconCondition2, icon);
+
+                        ViewGroup.LayoutParams layoutParams = iconCondition2.getLayoutParams();
+                        layoutParams.width = 200;
+                        layoutParams.height = 200;
+                        iconCondition2.setLayoutParams(layoutParams);
+
+                        final LinearLayout linearLayout3 = new LinearLayout(MainActivity.this);
+                        linearLayout3.setOrientation(LinearLayout.VERTICAL);
+
+                        TextView description = new TextView(MainActivity.this);
+                        description.setText(Description);
+                        description.setPadding(10, 5, 5, 5);
+                        description.setTextColor(Color.WHITE);
+                        description.setGravity(Gravity.CENTER);
+                        description.setTextSize(16);
+
+                        linearLayout.addView(min);
+                        linearLayout.addView(max);
+                        linearLayout2.addView(iconCondition2);
+                        linearLayout3.addView(description);
+
+
+                        ContextThemeWrapper ctw = new ContextThemeWrapper(MainActivity.this, R.style.Theme_AppCompat_Dialog);
+                        AlertDialog.Builder builder = new AlertDialog.Builder(ctw);
+
+
+                        TextView TitleView = new TextView(MainActivity.this);
+                        String out = "Weather for " + Day + ", " + Date;
+                        TitleView.setText(out);
+                        TitleView.setTextColor(Color.WHITE);
+                        TitleView.setTextSize(20);
+                        TitleView.setPadding(0, 15, 0, 15);
+                        TitleView.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+                        TitleView.setBackgroundColor(Color.parseColor("#80002afe"));
+                        builder.setCustomTitle(TitleView);
+
+                        final LinearLayout linearLayout4 = new LinearLayout(MainActivity.this);
+                        linearLayout4.setOrientation(LinearLayout.HORIZONTAL);
+                        linearLayout4.setGravity(Gravity.CENTER);
+                        linearLayout4.addView(linearLayout);
+                        linearLayout4.addView(linearLayout2);
+                        linearLayout4.addView(linearLayout3);
+
+                        builder.setCancelable(true);
+                        builder.setView(linearLayout4);
+
+                        final AlertDialog alert = builder.create();
+                        alert.show();
+
+
+                    }
+                });
+
+
+                final LinearLayout verticalBorder = new LinearLayout(MainActivity.this);
+                verticalBorder .setOrientation(LinearLayout.VERTICAL);
+                verticalBorder.setBackgroundColor(Color.WHITE);
+                verticalBorder.setLayoutParams(new TableRow.LayoutParams(1,TableRow.LayoutParams.MATCH_PARENT));
+
+                TextView border = new TextView(MainActivity.this);
+                description.setText("");
+                description.setTextColor(Color.WHITE);
+                description.setGravity(Gravity.CENTER);
+                description.setTextSize(16);
+
+                verticalBorder.addView(border);
+                tr.addView(linearLayout);
+
+                /*
+                    For no border at the end
+                 */
+                if(i !=5)
+                {
+                    tr.addView(verticalBorder);
+                }
+
+
+
+
+            }
+
+
+
+            tblForecast.addView(tr);
+
+
+        }
+        catch (Exception e)
+        {
+            Toast.makeText(MainActivity.this,"Please Refresh",Toast.LENGTH_LONG).show();
+        }
+
+            MainActivity.load.setVisibility(View.GONE);
+
+    }
 }
--- WeatherNow/app/src/main/java/com/govenderaneshen/weathernow/RequestWeatherData.java
+++ WeatherNow/app/src/main/java/com/govenderaneshen/weathernow/RequestWeatherData.java
@@ -1,20 +1,11 @@
 package com.govenderaneshen.weathernow;
 
-import android.app.AlertDialog;
 import android.content.Context;
-import android.graphics.Color;
 import android.os.AsyncTask;
-import android.view.ContextThemeWrapper;
-import android.view.Gravity;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TableRow;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
@@ -23,10 +14,6 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.text.SimpleDateFormat;
-import java.util.Locale;
-
-import static com.govenderaneshen.weathernow.MainActivity.date;
 
 /**
  * @author Govender Aneshen
@@ -43,80 +30,18 @@ public class RequestWeatherData extends AsyncTask <String,Void,String>
     private URL weatherURL;
     private HttpURLConnection urlConnection = null;
     private Context thisContext;
-    public String areaName;
     private weather[] forecast;
-    public AlertDialog alert;
-    public AlertDialog.Builder builder;
+    private AccessData dataOut;
+    public String areaName;
 
 
     public RequestWeatherData(Context context)
     {
         thisContext = context;
+        dataOut = (AccessData)context;
     }
 
-    public void setImage(ImageView imgCondition, String conditionsCode)
-    {
-        /*
-         * Switch statement to allocate the correct icon based on the weather condition
-         */
 
-        switch (conditionsCode) {
-            case "01d":
-                imgCondition.setImageResource(R.drawable.sun);
-                break;
-            case "01n":
-                imgCondition.setImageResource(R.drawable.moon);
-                break;
-            case "02d":
-                imgCondition.setImageResource(R.drawable.daycloud);
-                break;
-            case "02n":
-                imgCondition.setImageResource(R.drawable.nightcloud);
-                break;
-            case "03d":
-                imgCondition.setImageResource(R.drawable.cloud);
-                break;
-            case "03n":
-                imgCondition.setImageResource(R.drawable.cloud);
-                break;
-            case "04d":
-                imgCondition.setImageResource(R.drawable.brokencloud);
-                break;
-            case "04n":
-                imgCondition.setImageResource(R.drawable.brokencloud);
-                break;
-            case "09d":
-                imgCondition.setImageResource(R.drawable.showers);
-                break;
-            case "09n":
-                imgCondition.setImageResource(R.drawable.showers);
-                break;
-            case "10d":
-                imgCondition.setImageResource(R.drawable.rainday);
-                break;
-            case "10n":
-                imgCondition.setImageResource(R.drawable.rainnight);
-                break;
-            case "11d":
-                imgCondition.setImageResource(R.drawable.thunder);
-                break;
-            case "11n":
-                imgCondition.setImageResource(R.drawable.thunder);
-                break;
-            case "13d":
-                imgCondition.setImageResource(R.drawable.snow);
-                break;
-            case "13n":
-                imgCondition.setImageResource(R.drawable.snow);
-                break;
-            case "50d":
-                imgCondition.setImageResource(R.drawable.mist);
-                break;
-            case "50n":
-                imgCondition.setImageResource(R.drawable.mist);
-                break;
-        }
-    }
 
     /**
      * Used to retrieve the weather data
@@ -152,10 +77,6 @@ public class RequestWeatherData extends AsyncTask <String,Void,String>
 
             }
 
-            ContextThemeWrapper ctw = new ContextThemeWrapper(thisContext, R.style.Theme_AppCompat_Dialog);
-            builder = new AlertDialog.Builder(ctw);
-
-            MainActivity.stringWeatherData =weatherResults;
 
             return weatherResults;
 
@@ -190,12 +111,13 @@ public class RequestWeatherData extends AsyncTask <String,Void,String>
     protected void onPostExecute(String result) {
         super.onPostExecute(result);
 
+
+
         try
         {
             /*
                 Main JSON object from the OpenWeatherApp API
             */
-
             JSONObject jsresponse = new JSONObject(result.substring(result.indexOf("{"),result.lastIndexOf("}")+1));
 
             /*
@@ -204,6 +126,7 @@ public class RequestWeatherData extends AsyncTask <String,Void,String>
             JSONArray jsonList = jsresponse.optJSONArray("list");
             forecast = new weather[jsonList.length()];
             JSONObject jsonCity = jsresponse.getJSONObject("city");
+            areaName = jsonCity.getString("name");
             TemperatureConversions tempCelsius = new TemperatureConversions();
 
             for(int i =0;i<jsonList.length();i++)
@@ -226,148 +149,8 @@ public class RequestWeatherData extends AsyncTask <String,Void,String>
 
             }
 
+            dataOut.returnData(forecast,areaName);
 
-            /*
-                Setting text in textViews
-             */
-            areaName = jsonCity.getString("name");
-            MainActivity.minTempView.setText("Min:  "+forecast[0].getTempMin()+ " °C");
-            MainActivity.maxTempView.setText("Max: "+forecast[0].getTempMax()+ " °C");
-            MainActivity.ConditionView.setText(forecast[0].getWeatherDescription());
-            MainActivity.AreaView.setText(areaName);
-            setImage(MainActivity.condition,forecast[0].getConditionsCode());
-
-            SimpleDateFormat dateFormatday = new SimpleDateFormat("dd-MM-yyyy HH:mm", Locale.getDefault());
-            dateFormatday.applyPattern("EEEE, dd MMM yyyy");
-            date.setText(dateFormatday.format(forecast[0].getDate()));
-
-
-            /*
-                Dealing with the forecast for 5 days
-             */
-            MainActivity.tblForecast.removeAllViewsInLayout();
-
-            for(int i =1;i<jsonList.length();i++)
-            {
-                TableRow tr = new TableRow(thisContext);
-
-                /*
-                    Storing data from the forecast array into local variables
-                 */
-                String Day = forecast[i].getDay();
-                SimpleDateFormat dateFormatday2 = new SimpleDateFormat("dd-MM-yyyy HH:mm", Locale.getDefault());
-                dateFormatday2.applyPattern("dd MMM");
-                String Date= dateFormatday2.format(forecast[i].getDate());
-                String icon = forecast[i].getConditionsCode();
-                String Description = forecast[i].getWeatherDescription();
-                int AverageTemp = (forecast[i].getTempMin() +forecast[i].getTempMax())/2;
-
-                TextView day = new TextView(thisContext);
-                day.setText(Day);
-                day.setPadding(10, 5, 5, 5);
-                day.setTextColor(Color.WHITE);
-                day.setBackgroundColor(Color.parseColor("#80002afe"));
-                day.setGravity(Gravity.CENTER);
-                day.setTextSize(16);
-
-                TextView date2 = new TextView(thisContext);
-                date2.setText(Date);
-                date2.setPadding(10, 5, 5, 5);
-                date2.setTextColor(Color.WHITE);
-                date2.setBackgroundColor(Color.parseColor("#80002afe"));
-                date2.setGravity(Gravity.CENTER);
-                date2.setTextSize(16);
-
-                final int index = i;
-                TextView temp = new TextView(thisContext);
-                temp.setText(AverageTemp + " °C" );
-                temp.setPadding(10, 5, 5, 5);
-                temp.setTextColor(Color.BLACK);
-                temp.setGravity(Gravity.CENTER);
-                temp.setTextSize(16);
-
-                TextView description = new TextView(thisContext);
-                description.setText(Description);
-                description.setPadding(10, 5, 5, 5);
-                description.setTextColor(Color.BLACK);
-                description.setGravity(Gravity.CENTER);
-                description.setTextSize(16);
-
-
-
-                ImageView iconCondition = new ImageView(thisContext);
-
-                iconCondition.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT,TableRow.LayoutParams.WRAP_CONTENT));
-                setImage(iconCondition,icon);
-
-                ViewGroup.LayoutParams layoutParams = iconCondition.getLayoutParams();
-                layoutParams.width = 80;
-                layoutParams.height = 80;
-                iconCondition.setLayoutParams(layoutParams);
-
-                final LinearLayout linearLayout = new LinearLayout(thisContext);
-                TextView hello;
-                hello = new TextView(thisContext);
-                hello.setText("hello");
-                linearLayout.addView(hello);
-
-//                linearLayout.addView(temp);
-//                linearLayout.addView(description);
-//                linearLayout.addView(iconCondition);
-
-                temp.setOnClickListener(new  View.OnClickListener()
-                {
-                    @Override
-                    public void onClick(View v)
-                    {
-
-
-
-
-
-
-                        TextView TitleView = new TextView(thisContext);
-                        TitleView.setText("Weather for " + forecast[index].getDate().toString());
-                        TitleView.setTextColor(Color.WHITE);
-                        TitleView.setTextSize(20);
-                        TitleView.setPadding(0, 15, 0, 15);
-                        TitleView.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
-                        builder.setCustomTitle(TitleView);
-
-
-                        builder.setCancelable(true);
-                        builder.setView(linearLayout);
-
-                        alert = builder.create();
-                        alert.show();
-
-
-
-
-
-
-                    }
-                });
-
-
-
-
-
-                tr.addView(day);
-                tr.addView(date2);
-                tr.addView(temp);
-                tr.addView(description);
-                tr.addView(iconCondition);
-
-
-                MainActivity.tblForecast.addView(tr);
-            }
-
-
-
-
-
-            MainActivity.load.setVisibility(View.GONE);
 
 
 
@@ -376,11 +159,11 @@ public class RequestWeatherData extends AsyncTask <String,Void,String>
         /*
             Error with JSON result
          */
-        catch (JSONException e) {
-            e.printStackTrace();
+        catch (Exception e)
+        {
+            MainActivity.load.setVisibility(View.GONE);
+            Toast.makeText(thisContext,"Please Refresh",Toast.LENGTH_LONG).show();
         }
-
-
 
 
     }
